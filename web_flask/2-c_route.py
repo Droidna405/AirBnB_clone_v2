fa06193233b45c739c274dc97f7abffdcf1999f9
Droidna405 <joemwangala@gmail.com>
2024-08-05 15:21:24 +0300

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,12 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Returns a string 'Hello HBNB!' when accessing the root URL
     """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
@@ -20,6 +22,7 @@ def hbnb():
     """
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
@@ -27,6 +30,7 @@ def c_text(text):
     with underscores replaced by spaces
     """
     return "C " + text.replace('_', ' ')
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
